2b3b881cc4e6c4112958b505194dfcb724bb9f38
Cheezzyy1 <acapulcojohncedric66@gmail.com>
2026-02-13 01:33:52 -0800

    routes fixed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mail = Mail(app)
 initialize_firebase_admin()
 
 # Import routes
-from routes import main_routes, api_routes, municipal_routes, seminar_routes, service_routes, fisheries_routes, environment_routes, forest_routes, livestock_routes, permits_routes, wildlife_routes, farm_routes, payments_routes,regional_routes,superadmin_routes
+from routes import main_routes, api_routes, municipal_routes, seminar_routes, service_routes, fisheries_routes, environment_routes, forest_routes, livestock_routes, permits_routes, wildlife_routes, farm_routes, payments_routes,regional_routes,superadmin_routes, national_routes
 
 # Initialize mail in api_routes
 api_routes.init_mail(mail)
@@ -34,6 +34,7 @@ app.register_blueprint(farm_routes.bp)
 app.register_blueprint(payments_routes.bp)
 app.register_blueprint(regional_routes.bp)
 app.register_blueprint(superadmin_routes.bp)
+app.register_blueprint(national_routes.bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ routes/national_routes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('national', __name__, url_prefix='/national')
+
+@bp.route('/application-national')
+def application_national_view():
+    return render_template('national/applications-national.html')
+
+@bp.route('/permit-national')
+def permit_national_view():
+    return render_template('national/licensing-permit-national.html')
+
+@bp.route('/service-national')
+def service_national_view():
+    return render_template('national/service-national.html')
+
+@bp.route('/inventory-national')
+def inventory_national_view():
+    return render_template('national/inventory-national.html')
+
+@bp.route('/user-inventory-national')
+def user_inventory_national_view():
+    return render_template('national/user-inventory-national.html')
+
+@bp.route('/transaction-national')
+def transaction_national_view():
+    return render_template('national/transaction-national.html')
+
+@bp.route('/user-management-national')
+def user_management_national_view():
+    return render_template('national/transaction-national.html')
+
+@bp.route('/profile-national')
+def profile_national_view():
+    return render_template('national/profile-national.html')
